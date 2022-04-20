b5b201caa689e19a05b918578ef07fce578c6731
Vaishnavi <vaishnavik0901@gmail.com>
2022-04-20 23:16:50 +0530

    OpenEMR

---
+++ src/test/java/com/harman/test/LoginTest.java
@@ -0,0 +1,34 @@
+package com.harman.test;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.harman.base.WebDriverWrapper;
+import com.harman.utilities.DataUtils;
+	
+public class LoginTest extends WebDriverWrapper {
+	
+	
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "validCredentialData")
+	public void validCredentialTest() {
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.cssSelector("#clearPass")).sendKeys("pass");
+		driver.findElement(By.id("login-button")).click();
+
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, "OpenEMR");
+	}
+
+	@Test
+	public void invalidCredentialTest() {
+		driver.findElement(By.id("authUser")).sendKeys("john");
+		driver.findElement(By.cssSelector("#clearPass")).sendKeys("john123");
+		driver.findElement(By.id("login-button")).click();
+
+		String actualError = driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, "Invalid username or password");
+	}
+
+}
